0a228539af00aa1588f7aa35150d69e5e3c273ef
william <espiw@farmingdale.edu>
2026-02-02 18:55:09 -0500

    declared our functions

--- my-react-app-stopwatch/src/StopWatch.jsx
+++ my-react-app-stopwatch/src/StopWatch.jsx
@@ -11,6 +11,32 @@ function StopWatch() {
     // we need to get current time when we start our stopwatch
     const startTimeRef = useRef(0);
 
+    // when we mount our component and if isRunning ever changes, we perform whatever code is in useEffect
+    // this is where we start our interval (move time forward)
+    useEffect(() => {
+
+    }, [isRunning]);
+
+    // this function will handle starting the time
+    function start() {
+
+    }
+
+    // this function will handle stopping the time
+    function stop() {
+
+    }
+
+    // this function will handle resetting the time
+    function reset() {
+
+    }
+
+    // this function will handle formatting the time
+    function formatTime() {
+
+    }
+
     return(
         <p>hello</p>
     );
